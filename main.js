5f71a7f290295634095f9c7ae83b6d27ba523c66
Roberto <robertogarcia287@gmail.com>
2023-08-03 21:53:57 -0500

    Finding functions

--- main.js
+++ main.js
@@ -7,9 +7,24 @@ fetch('http://localhost:3000/art')
 
 //The hovering event
 
-//Grabbing the name
+//Grabbing the names
 
+
+//Function that finds the name
+function artName (obj) {
+    return newNames[obj.name]
+}
 
 //The click event
 
 //Grabbing the rest of the data from JSON
+
+//Function that find the descriptions
+function artInfo (obj) {
+    return newInfo[obj.description]
+}
+
+//Function that finds the images
+function artImage (obj) {
+    return newImages[obj.image]
+}
