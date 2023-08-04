4b4ceb3270e472ad42c57fb7dd3f251e304edda8
Roberto <robertogarcia287@gmail.com>
2023-08-03 20:57:06 -0500

    Edited java

--- main.js
+++ main.js
@@ -1,30 +1,15 @@
 console.log('main.js is connected')
+fetch('http://localhost:3000/art')
+.then(res => console.log(res))
 
-let slideIndex = 1;
-showSlides(slideIndex);
+//Timeout for name appearing
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+//The hovering event
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+//Grabbing the name
+
+
+//The click event
+
+//Grabbing the rest of the data from JSON
