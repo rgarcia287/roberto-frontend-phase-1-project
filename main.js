f7a8b3e88622186af3bad7a23c0693f12d7ee482
Roberto <robertogarcia287@gmail.com>
2023-08-06 17:04:19 -0500

    edited java

--- main.js
+++ main.js
@@ -9,28 +9,42 @@ fetch('http://localhost:3000/art')
     renderArtCard(data)
   });
 
-  //Calling Functions
-  renderArtCard()
 
   //Elements
   const artContainer = document.querySelector(".art-container")
 
   //Card
   function renderArtCard (data) {
-    const artCard = document.createElement('div')
-    artCard.classList.add("card")
-    artCard.addEventListener("click", e => infoClick())
-    artCard.addEventListener ("mouseenter", (event) => {
-        data.forEach(data => {
-            data.name
-        });
-        setTimeout(() => {
-            event.target.e = "";
-        }, 1000);
-    },
-  )
+    data.forEach(art => {
+        const artCard = document.createElement('div')
+        artCard.classList.add("card")
+        const artImage = document.createElement('img')
+        artImage.src = art.image
+       
+        artCard.addEventListener("click", e => {
+            console.log("Clicked!")
+    })
+    artCard.append(artImage)
+    artContainer.append(artCard)
+    })
+
+
+   //     data.description
+//     })
+//     artCard.addEventListener ("mouseenter", (event) => {
+//         data.forEach(e => {
+//             data.name
+//         });
+//         setTimeout(() => {
+//             event.target.e = "";
+//         }, 1000);
+//     },
+//   )
 
     const image = document.createElement("img")
-    image.src = data.forEach(data => {
+    image.src = data.forEach(e => {
         data.image}) 
+
+    artCard.append(image)
+    artContainer.append(artCard)
 }
